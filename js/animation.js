128e7aa75ab5fcbe7cfab1a98e13b3f156d20b71
vumg <ab17100222@gmail.com>
2019-08-28 01:11:15 -0500

    Primer maqueta

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,36 @@
+//animation properties
+
 var elipse = document.getElementsByClassName("elipse");
-var elipse2 = document.getElementsByClassName("elipse2");
-var elipse3 = document.getElementsByClassName("elipse3");
 
-var opacity, scale, repeat = 0;
+var opacity,
+  scale,
+  repeat = 0;
 
-opacity = .1;
-scale = 1.2; 
+opacity = 0.1;
+scale = 1.2;
 repeat = -1;
 
-TweenMax.to(elipse, 2, { opacity: opacity, scale: scale,repeat:repeat}, 0.7);
-TweenMax.to(elipse2, 2, { opacity: opacity, scale: scale,repeat:repeat}, 0.7);
-TweenMax.to(elipse3, 2, { opacity: opacity, scale: scale,repeat:repeat}, 0.7);
+TweenMax.to(elipse, 2, { opacity: opacity, scale: scale, repeat: repeat });
 
+//scroll properties
+
+new fullpage("#fullPage", {
+  //navegacion
+  autoScrolling: true,
+  controlArrows: true,
+  navigation: true,
+  navigationPosition: "left",
+  scrollingSpeed: 800,
+
+  //diseño
+  verticalCentered: false,
+  paddingTop: "0",
+  paddingBottom: "0",
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+  responsiveSlides: false,
+  parallax: false,
+  parallaxOptions: { type: "reveal", percentage: 62, property: "translate" },
+  cards: false,
+  cardsOptions: { perspective: 100, fadeContent: false, fadeBackground: false }
+});
